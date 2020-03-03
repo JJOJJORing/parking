dcc710a1a0aaf02adc936a64ab7cb83b10f2c3cd
hanrimJO <whgksfla@gmail.com>
2020-03-03 20:37:02 +0900

    Add view, another templates

--- parking/urls.py
+++ parking/urls.py
@@ -5,5 +5,8 @@ app_name = 'parking'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('car_in/', views.car_in, name='car_in')
+    path('car_in/', views.car_in, name='car_in'),
+    path('car_out/', views.car_out, name='car_out'),
+    path('success_in/', views.success_in, name='success_in'),
+    path('success_out/', views.success_out, name='success_out'),
 ]
--- parking/views.py
+++ parking/views.py
@@ -1,47 +1,67 @@
 from django.shortcuts import render, redirect
 from parking.forms import *
 from parking.models import Car, User, Log
+from django.core.exceptions import ObjectDoesNotExist
 import math
-from django.http import HttpResponse
+
+
+def index(request):
+    form = LogForm()
+    return render(request, 'parking/index.html', {'form': form})
+
+
+def success_in(request):
+    return render(request, 'parking/success_in.html', {})
+
+
+def success_out(request):
+    return render(request, 'parking/success_out.html', {})
 
 
 def car_in(request):
     if request.method == "POST":
-        if request.POST.get('carin') == '입차':
-            form = LogForm(request.POST)
-            if form.is_valid():
-                log = form.save(commit=False)
-
-                user_car = Car.objects.filter(car_num=log.car_number)
-                log_latest = Log.objects.filter(car_number=log.car_number).order_by('-car_in')[0]
-                if log_latest.pay_val:
-                    if user_car.filter(ticket_limit__gt=timezone.now()):
-                        log.user_stat = True
-                    else:
-                        log.user_stat = False
-                    log.save()
-                    return redirect('success_in/')
-                else:
-                    return HttpResponse('오류입니다')
-        else:
-            form = LogForm(request.POST)
-            if form.is_valid():
-                log = form.save(commit=False)
-                log_recent = Log.objects.filter(car_number=log.car_number).order_by('-car_out')[0]
-                log_recent.car_out = timezone.now()
-                out_time = log_recent.car_out
-                log_recent.save()
-
-                if log_recent.user_stat and log_recent.pay_val is None:
-                    log_recent.pay_val = 'ticket'
-                    log_recent.save()
-                    return redirect('success_out/')
-                else:
-                    in_time = log_recent.car_in
-                    value = out_time - in_time
-                    log_recent.pay_val = math.ceil(value.seconds/60)*10
-                    log_recent.save()
-                    return redirect('success_out/')
+        form = LogForm(request.POST)
+        if form.is_valid():
+            log = form.save(commit=False)
+            try:
+                user_car = Car.objects.get(car_num=log.car_number, ticket_limit__gte=timezone.now())
+                log.user_stat = True
+                log.pay_val = 'ticket'
+            except ObjectDoesNotExist as e:
+                log.user_stat = False
+            log.car_stat = True
+            log.save()
+            return redirect('http://127.0.0.1:8000/parking/success_in/')
     else:
         form = LogForm()
-    return render(request, 'parking/index.html', {'form': form})
+        return render(request, 'parking/index.html', {'form': form})
+
+
+def car_out(request):
+    if request.method == 'POST':
+        form = LogForm(request.POST)
+        if form.is_valid():
+            try:
+                log = form.save(commit=False)
+                log_recent = Log.objects.get(car_number=log.car_number, car_stat=True)
+                user = log_recent.user_stat
+                if user:
+                    log_recent.car_out = timezone.now()
+                    log_recent.car_stat = False
+                    log_recent.save()
+                else:
+                    out_time = timezone.now()
+                    in_time = log_recent.car_in
+                    value = math.ceil((out_time - in_time).seconds / 60) * 100
+                    log_recent.car_out = out_time
+                    log_recent.car_stat = False
+                    log_recent.pay_val = value
+                    log_recent.save()
+
+                return redirect('http://127.0.0.1:8000/parking/success_out/')
+            except Exception as e:
+                print(e)
+                return redirect('http://127.0.0.1:8000/parking/')
+    else:
+        form = LogForm(request.POST)
+        return render(request, 'parking/index.html', {'form': form})
