a4ac15c1214e42753a305869c4c3133361917f13
hanrimJO <whgksfla@gmail.com>
2020-03-03 20:08:33 +0900

    Add model list in admin

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from parking.models import *
+
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'phone', 'email']
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'car_num', 'ticket_num', 'ticket_limit', 'user']
+
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'car_number', 'car_in', 'car_out', 'user_stat', 'pay_val', 'car_stat']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Car, CarAdmin)
+admin.site.register(Log, LogAdmin)
