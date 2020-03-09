65b0ee57e1096bc55eec58be6320118b731425ee
hanrimJO <whgksfla@gmail.com>
2020-03-09 13:39:52 +0900

    fix html

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#rip1z1rnqh@n)mmk!rqm&#sn3na&z59%2nhf4!3z7lebcwbxs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.64']
 
 
 # Application definition
